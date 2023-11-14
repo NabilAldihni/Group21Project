9e88a9ade4cf9326679ddc8025a2eefc3da48adb
NabilAldihni <aldihninabil@gmail.com>
2023-11-14 00:08:20 -0500

    Removed redundant imports

--- app/src/main/java/com/example/group21project/MainActivity.java
+++ app/src/main/java/com/example/group21project/MainActivity.java
@@ -4,15 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 

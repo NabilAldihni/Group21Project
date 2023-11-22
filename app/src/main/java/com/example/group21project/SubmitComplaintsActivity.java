f9b99f9358e42c3ba15677e894424fea050e96d1
NabilAldihni <aldihninabil@gmail.com>
2023-11-22 12:06:04 -0500

    Non-working firebase complaints code

--- app/src/main/java/com/example/group21project/SubmitComplaintsActivity.java
+++ app/src/main/java/com/example/group21project/SubmitComplaintsActivity.java
@@ -1,15 +1,24 @@
 package com.example.group21project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SubmitComplaintsActivity extends AppCompatActivity {
 
@@ -24,16 +33,42 @@ public class SubmitComplaintsActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 // Upload complaint to Firebase
-                // text is stored in complaintTextInput.getText()
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-                Snackbar snackbar = Snackbar.make(v, "Your complaint has been submitted", Snackbar.LENGTH_LONG);
-                snackbar.setAction("Dismiss", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        snackbar.dismiss();
-                    }
-                });
-                snackbar.show();
+                Map<String, Object> complaint = new HashMap<>();
+                complaint.put("description", complaintTextInput.getText().toString());
+
+                // Add a new document with a generated ID
+                db.collection("complaints")
+                        .add(complaint)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Snackbar snackbar = Snackbar.make(v, "Your complaint has been submitted", Snackbar.LENGTH_LONG);
+                                snackbar.setAction("Dismiss", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        snackbar.dismiss();
+                                    }
+                                });
+                                snackbar.show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar snackbar = Snackbar.make(v, "Something went wrong, please try again", Snackbar.LENGTH_LONG);
+                                snackbar.setAction("Dismiss", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        snackbar.dismiss();
+                                    }
+                                });
+                                snackbar.show();
+                            }
+                        });
+
+
             }
         });
     }

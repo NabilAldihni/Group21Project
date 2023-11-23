34c8701802380a4630234d96093734ac820633a7
Shak789 <shakjivraj87@gmail.com>
2023-11-23 15:33:51 -0500

    front page for post

--- app/src/main/java/com/example/group21project/MainActivity.java
+++ app/src/main/java/com/example/group21project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.group21project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        TextView mTitle = toolbar.findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        mTitle.setText(toolbar.getTitle());
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
+

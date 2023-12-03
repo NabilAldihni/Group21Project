9c3fc30ec9c9475a058b891f411d26e3dd0409c5
Mani Bondari <manibondari@gmail.com>
2023-12-03 18:01:07 -0500

    added to the new UserCompat class with helpful methods for reusability

--- app/src/main/java/com/example/group21project/UserCompat.java
+++ app/src/main/java/com/example/group21project/UserCompat.java
@@ -5,6 +5,8 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Random;
+import java.util.Base64;
 
 public class UserCompat {
     public static int calculateAge(String birthdateStr) {
@@ -26,6 +28,36 @@ public class UserCompat {
             return false;
         }
         Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static String generateRandomUsername(String name) {
+        Random random = new Random();
+        int number = random.nextInt(10000);
+        return name + number;
+    }
+
+    // Method to convert Celsius to Fahrenheit
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    // Method to check if a string is a palindrome
+    public static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    // Method to encrypt a string (basic encoding)
+    public static String encryptString(String input) {
+        return Base64.getEncoder().encodeToString(input.getBytes());
+    }
+
+    // Method to validate a phone number
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        String phoneRegex = "^[+]?[0-9]{10,13}$";
+        Pattern pattern = Pattern.compile(phoneRegex);
+        Matcher matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
 }
